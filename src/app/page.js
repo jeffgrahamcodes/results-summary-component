ce5f13dbb38984a621aafb5bef4d56f4a3034f87
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-04-11 00:41:18 -0400

    add gradient

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,47 @@
 import Image from 'next/image';
+import data from '../data.json';
 
 export default function Home() {
   return (
-    <div>
-      <main className="h-screen flex justify-center items-center">
-        <section>
+    <div className="h-screen  bg-(--very-light-blue) flex flex-col justify-center items-center">
+      <main className=" bg-white flex flex-row rounded-4xl w-[736px] box-border h-[512px]">
+        <section className="bg-gradient-to-b from-[#7755FF] from-0% via-[#6943FF] via-0% to-[#2F2CE9] to-100% rounded-4xl w-[368px] flex flex-col items-center justify-evenly text-center">
           <p>Your Result</p>
-          <p>76 of 100</p>
-          <p>Great</p>
-          <p>
-            You scored higher than 65% of the people who have taken
-            these tests.
+          <p className="w-[200px] h-[200px] flex justify-center items-center rounded-full bg-gradient-to-b from-[#7755FF] from-0% via-[#6943FF] via-0% to-[#2F2CE9] to-100% ">
+            76 of 100
           </p>
+          <div className="w-[260px] h-[134px]">
+            <p className="mb-[14px]">Great</p>
+            <p>
+              You scored higher than 65% of the people who have taken
+              these tests.
+            </p>
+          </div>
         </section>
-        <section>
+        <section className="flex flex-col justify-evenly text-black w-[368px] px-[40px]">
           <p>Summary</p>
-          <p>Reaction 80 / 100</p>
-          <p>Memory 92 / 100</p>
-          <p>Verbal 61 / 100</p>
-          <p>Visual 72 / 100</p>
-          <p>Continue</p>
+          <ul className="grid gap-[16px]">
+            {data.map((d) => (
+              <li
+                key={d.category}
+                className="border w-[288px] h-[56px] rounded-xl"
+              >
+                <Image
+                  src={`${d.icon}`}
+                  width={20}
+                  height={20}
+                  alt=""
+                  className="inline"
+                />
+                {d.category} {d.score}/100
+              </li>
+            ))}
+          </ul>
+          <button className="w-[288px] h-[56px]">Continue</button>
         </section>
       </main>
 
-      <footer class="attribution">
+      <footer className="absolute inset-x-0 bottom-0 text-center">
         Challenge by
         <a
           href="https://www.frontendmentor.io?ref=challenge"
@@ -31,7 +49,7 @@ export default function Home() {
         >
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by <a href="#">Jeff Graham</a>.
       </footer>
     </div>
   );
