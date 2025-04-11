6a83f00c92d0a5a8278a4c00e82322912b7c83c2
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-04-11 11:01:20 -0400

    add some responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,26 @@ import data from '../data.json';
 export default function Home() {
   return (
     <div className="h-screen  bg-(--very-light-blue) flex flex-col justify-center items-center">
-      <main className=" bg-white flex flex-row rounded-4xl w-[736px] box-border h-[512px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+      <main className=" bg-white flex flex-col sm:flex-row rounded-4xl w-fit sm:w-[736px] box-border h-[100vh] sm:h-[512px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
         <section className="bg-gradient-to-b from-[#7755FF] from-0% via-[#6943FF] via-0% to-[#2F2CE9] to-100% rounded-4xl w-[368px] flex flex-col items-center justify-evenly text-center">
           <p className="heading-m text-(--light-blue)">Your Result</p>
           <span className="w-[200px] h-[200px] flex flex-col justify-center items-center rounded-full bg-gradient-to-b from-[#7755FF] from-0% via-[#6943FF] via-0% to-[#2F2CE9] to-100% ">
-            <p className="heading-xl">76</p>
+            <p className="heading-xl text-white">76</p>
             <p className="text-lg font-bold text-(--light-blue) opacity-[.5168]">
               of 100
             </p>
           </span>
           <div className="w-[260px] h-[134px]">
-            <p className="mb-[14px] heading-l font-bold">Great</p>
+            <p className="mb-[14px] heading-l font-bold text-white">
+              Great
+            </p>
             <p className="text-lg font-medium text-(--light-blue) leading-[22px]">
               You scored higher than 65% of the people who have taken
               these tests.
             </p>
           </div>
         </section>
-        <section className="flex flex-col justify-evenly text-black w-[368px] px-[40px]">
+        <section className="flex flex-col justify-evenly h-[100%] text-black w-[368px] px-[40px]">
           <p className="heading-m text-(--dark-navy)">Summary</p>
           <ul className="grid gap-[16px]">
             {data.map((d) => (
